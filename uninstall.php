31942e7377a077d41e843048ba44caeb23d06dba
Till Krüss <till@kruss.io>
2016-09-18 19:48:04 -0700

    Flush cache before uninstalling plugin

--- uninstall.php
+++ uninstall.php
@@ -8,6 +8,8 @@ ob_start();
 
 if ( file_exists( WP_CONTENT_DIR . '/object-cache.php' ) && method_exists( $GLOBALS[ 'wp_object_cache' ], 'redis_status' ) ) {
 
+	wp_cache_flush();
+
 	if ( WP_Filesystem( request_filesystem_credentials( '' ) ) ) {
 		$wp_filesystem->delete( WP_CONTENT_DIR . '/object-cache.php' );
 	}
