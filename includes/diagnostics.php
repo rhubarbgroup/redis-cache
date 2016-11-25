664b9624368b6a27850ecb4027998cd9d2665e6b
Till Krüss <till@kruss.io>
2016-11-25 13:13:31 -0800

    Simplified and rearranged diagnostics

--- includes/diagnostics.php
+++ includes/diagnostics.php
@@ -1,6 +1,18 @@
 <?php
 
+global $wp_object_cache;
+
 $info = array();
+$dropin = $this->validate_object_cache_dropin();
+
+$info[ 'Status' ] = $this->get_status();
+$info[ 'Client' ] = $this->get_redis_client_name();
+
+$info[ 'Drop-in' ] = $dropin ? 'Valid' : 'Invalid';
+
+
+$info[ 'Redis Extension' ] = class_exists( 'Redis' ) ? phpversion( 'redis' ) : 'Not Found';
+$info[ 'Predis Client' ] = class_exists( 'Predis\Client' ) ? Predis\Client::VERSION : 'Not Found';
 
 if ( defined( 'PHP_VERSION' ) ) {
     $info[ 'PHP Version' ] = PHP_VERSION;
@@ -10,13 +22,12 @@ if ( defined( 'HHVM_VERSION' ) ) {
     $info[ 'HHVM Version' ] = HHVM_VERSION;
 }
 
-$info[ __( 'Multisite', 'redis-cache' ) ] = is_multisite() ? __( 'Yes', 'redis-cache' ) : __( 'No', 'redis-cache' );
+$info[ 'Multisite' ] = is_multisite() ? 'Yes' : 'No';
 
-$info[ __( 'Redis', 'redis-cache' ) ] = class_exists( 'Redis' ) ? phpversion( 'redis' ) : __( 'Not Found', 'redis-cache' );
-$info[ __( 'Predis', 'redis-cache' ) ] = class_exists( 'Predis\Client' ) ? Predis\Client::VERSION : __( 'Not Found', 'redis-cache' );
-
-$info[ __( 'Status', 'redis-cache' ) ] = $this->get_status();
-$info[ __( 'Client', 'redis-cache' ) ] = $this->get_redis_client_name();
+if ( $dropin ) {
+	$info[ 'Global Prefix' ] = json_encode( $wp_object_cache->global_prefix );
+    $info[ 'Blog Prefix' ] = json_encode( $wp_object_cache->blog_prefix );
+}
 
 $constants = array(
     'WP_REDIS_DISABLED',
@@ -36,7 +47,7 @@ $constants = array(
 
 foreach ( $constants as $constant ) {
     if ( defined( $constant ) ) {
-        $info[$constant] = json_encode( constant( $constant ) );
+        $info[ $constant ] = json_encode( constant( $constant ) );
     }
 }
 
@@ -44,14 +55,9 @@ if ( defined( 'WP_REDIS_PASSWORD' ) ) {
     $info[ 'WP_REDIS_PASSWORD' ] = json_encode( empty( WP_REDIS_PASSWORD ) ? null : str_repeat( '*', strlen( WP_REDIS_PASSWORD ) ) );
 }
 
-if ( $this->validate_object_cache_dropin() ) {
-    $info[ __( 'Drop-in', 'redis-cache' ) ] = __( 'Valid', 'redis-cache' );
-    $info[ __( 'Global Prefix', 'redis-cache' ) ] = json_encode( $GLOBALS[ 'wp_object_cache' ]->global_prefix );
-    $info[ __( 'Blog Prefix', 'redis-cache' ) ] = json_encode( $GLOBALS[ 'wp_object_cache' ]->blog_prefix );
-    $info[ __( 'Global Groups', 'redis-cache' ) ] = json_encode( $GLOBALS[ 'wp_object_cache' ]->global_groups );
-    $info[ __( 'Ignored Groups', 'redis-cache' ) ] = json_encode( $GLOBALS[ 'wp_object_cache' ]->ignored_groups );
-} else {
-    $info[ __( 'Drop-in', 'redis-cache' ) ] = __( 'Invalid', 'redis-cache' );
+if ( $dropin ) {
+    $info[ 'Global Groups' ] = json_encode( $wp_object_cache->global_groups );
+    $info[ 'Ignored Groups' ] = json_encode( $wp_object_cache->ignored_groups );
 }
 
 foreach ($info as $name => $value) {
