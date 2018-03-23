cc92e6e2954f60fccdf9e3dc3e2076a416d19cd1
Till Krüss <till@kruss.io>
2018-03-22 18:31:42 -0700

    use full Predis library

--- includes/object-cache.php
+++ includes/object-cache.php
@@ -459,8 +459,7 @@ class WP_Object_Cache
                 // Load bundled Predis library
                 if (! class_exists('Predis\Client')) {
                     $plugin_dir = defined('WP_PLUGIN_DIR') ? WP_PLUGIN_DIR : WP_CONTENT_DIR . '/plugins';
-                    require_once $plugin_dir . '/redis-cache/includes/predis.php';
-                    Predis\Autoloader::register();
+                    require_once $plugin_dir . '/redis-cache/includes/predis/autoload.php';
                 }
 
                 $options = array();
