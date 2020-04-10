db58d65af5e696b22a7d808a9036c12609db0991
Nawawi Jamili <nawawi@rutweb.com>
2020-04-10 23:07:54 +0800

    fixed: when run "wp cache flush" and WP_REDIS_SELECTIVE_FLUSH set as true, the Redis fails to flush a cache due to salt contains a glob pattern. added: glob_quote.

--- includes/object-cache.php
+++ includes/object-cache.php
@@ -881,6 +881,10 @@ class WP_Object_Cache
         }
     }
 
+    protected function glob_quote($string) {
+        return str_replace(['[', '*', '?'], ['[[]', '[*]', '[?]'], $string);
+    }
+
     /**
      * Returns a closure ready to be called to flush selectively ignoring unflushable groups.
      *
@@ -889,6 +893,8 @@ class WP_Object_Cache
      */
     protected function lua_flush_closure($salt)
     {
+        $salt = $this->glob_quote($salt);
+
         return function () use ($salt) {
             $script = <<<LUA
                 local cur = 0
@@ -927,6 +933,8 @@ LUA;
      */
     protected function lua_flush_extended_closure($salt)
     {
+        $salt = $this->glob_quote($salt);
+
         return function () use ($salt) {
             $salt_length = strlen($salt);
 
