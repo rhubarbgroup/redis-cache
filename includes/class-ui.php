89db691a0cd1ae817c721c3bcae780c539b2b742
naxvog <naxvog@users.noreply.github.com>
2020-07-07 21:29:12 +0200

    added comments

--- includes/class-ui.php
+++ includes/class-ui.php
@@ -9,10 +9,26 @@ namespace Rhubarb\RedisCache;
 
 defined( '\\ABSPATH' ) || exit;
 
+/**
+ * UI class definition
+ */
 class UI {
 
+    /**
+     * Holds all registered tabs
+     *
+     * @var array
+     */
     private static $tabs = [];
 
+    /**
+     * Registers a settings tab
+     *
+     * @param string $slug   Unique slug to identify the tab.
+     * @param string $label  Tab label.
+     * @param array  $args   Optional arguments describing the tab.
+     * @return void
+     */
     public static function register_tab( $slug, $label, $args = [] ) {
         self::$tabs[ $slug ] = (object) wp_parse_args(
             $args,
@@ -26,6 +42,11 @@ class UI {
         );
     }
 
+    /**
+     * Retrieves all registered tabs
+     *
+     * @return array
+     */
     public static function get_tabs() {
         return self::$tabs;
     }
