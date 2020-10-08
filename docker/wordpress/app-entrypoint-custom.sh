4397936e86db18d224aa0501c10b28a72fb24bf4
naxvog <naxvog@users.noreply.github.com>
2020-10-08 21:27:08 +0200

    container generation updates

--- docker/wordpress/app-entrypoint-custom.sh
+++ docker/wordpress/app-entrypoint-custom.sh
@@ -17,17 +17,22 @@ fi
 PLUGIN_SOURCE_DIR="/redis-cache"
 PLUGIN_TARGET_DIR="/opt/bitnami/wordpress/wp-content/plugins/redis-cache"
 PHP_INI_PATH="/opt/bitnami/php/etc/php.ini"
+WP_DIR="/bitnami/wordpress"
+APF_FILE="/redis-cache/docker/apf.php"
 
 ## Symlink generation
+info "Creating plugin symlink"
 if [ ! -L "$PLUGIN_TARGET_DIR" ]; then
     ln -s "$PLUGIN_SOURCE_DIR" "$PLUGIN_TARGET_DIR"
 fi
 
 ## Set APF file
-if [ -f "/redis-cache/docker/apf.php" ]; then
+info "Setting PHP auto prened file to inject our plugin constants"
+if [ -f "$APF_FILE" ]; then
     cp "$PHP_INI_PATH" "$PHP_INI_PATH-original"
     TF=$(mktemp)
-    awk '{gsub(/^(auto_prepend_file\s*=\s*).+/,"& \"/redis-cache/docker/apf.php\"",$1)}1' "$PHP_INI_PATH" \
+    #awk '{gsub(/^(auto_prepend_file\s*=\s*).*/,"& \"/redis-cache/docker/apf.php\"",$1)}1' "$PHP_INI_PATH" \
+    awk '{gsub(/^auto_prepend_file =.*/,"auto_prepend_file = \"'"$APF_FILE"'\"")}1' "$PHP_INI_PATH" \
         > "$TF" \
         && mv "$TF" "$PHP_INI_PATH"
     info "Set PHP auto prepend file"
