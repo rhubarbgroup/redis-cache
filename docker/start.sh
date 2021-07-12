2a8afe760217742448ccbd894db595c6241bfa1f
naxvog <naxvog@users.noreply.github.com>
2021-07-12 18:07:18 +0200

    improved mode selection logic

--- docker/start.sh
+++ docker/start.sh
@@ -86,46 +86,53 @@ declare -A variables=();
 declare options=();
 declare -i index=1;
 
+variables["-m"]="mode";
+variables["--mode"]="mode";
 variables["-c"]="client";
 variables["--client"]="client";
 
-for i in "$@"; do
-  arguments[$index]=$i;
-  prev_index="$(expr $index - 1)";
+for i in ${@}; do
+    arguments[$index]=$i;
+    prev_index="$(expr $index - 1)";
 
-  # this if block does something akin to "where $i contains ="
-  # "%=*" here strips out everything from the = to the end of the argument leaving only the label
-  if [[ $i == *"="* ]]
-    then argument_label=${i%=*}
-    else argument_label=${arguments[$prev_index]}
-  fi
-
-  if [[ -n $argument_label ]] ; then
-    # this if block only evaluates to true if the argument label exists in the variables array
-    if [[ -n ${variables[$argument_label]} ]] ; then
-      # dynamically creating variables names using declare
-      # "#$argument_label=" here strips out the label leaving only the value
-      if [[ $i == *"="* ]]
-        then declare ${variables[$argument_label]}=${i#$argument_label=} 
-        else declare ${variables[$argument_label]}=${arguments[$index]}
-      fi
+    # this if block does something akin to "where $i contains ="
+    # "%=*" here strips out everything from the = to the end of the argument leaving only the label
+    if [[ $i == *"="* ]]; then
+        argument_label=${i%=*}
     else
-      # if the argument was not found store it in order
-      options+=(${arguments[$index]})
+        argument_label=${arguments[$prev_index]}
     fi
-  fi
 
-  index=index+1;
+    # first argument and no label detected: must be mode then
+    if [[ 1 == $index && -z $argument_label ]]; then
+        argument_label="-m"
+    fi
+
+    if [[ -n $argument_label ]]; then
+        # this if block only evaluates to true if the argument label exists in the variables array
+        if [[ -n ${variables[$argument_label]} ]]; then
+            # dynamically creating variables names using declare
+            # "#$argument_label=" here strips out the label leaving only the value
+            if [[ $i == *"="* ]]; then
+                declare ${variables[$argument_label]}=${i#$argument_label=} 
+            else
+                declare ${variables[$argument_label]}=${arguments[$index]}
+            fi
+        else
+          # if the argument was not found store it in order
+          options+=(${arguments[$index]})
+        fi
+    else
+      echo "unrecognized $1"
+    fi
+
+    index=index+1;
 done;
 
-mode="default"
-if [[ -n "$1" && "$1" != --* ]] ; then
-    mode="$1"
-fi
 
-case "$mode" in
+case ${mode-default} in
 
-    "default"|""|"-"|"simple"|"up"|"start")
+    "default"|"-"|"simple"|"up"|"start")
         start 1 0 0 "${options[@]}"
         apf --reset
         apf WP_REDIS_CLIENT "${client-phpredis}"
