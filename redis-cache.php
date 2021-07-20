feb0dce2f7062b9c81a2cdb2ac70c49ccb548f64
Till Krüss <till@kruss.io>
2021-07-20 12:26:44 -0700

    bump dev version

--- redis-cache.php
+++ redis-cache.php
@@ -3,7 +3,7 @@
  * Plugin Name: Redis Object Cache
  * Plugin URI: https://wordpress.org/plugins/redis-cache/
  * Description: A persistent object cache backend powered by Redis. Supports Predis, PhpRedis, Credis, HHVM, replication, clustering and WP-CLI.
- * Version: 2.0.21
+ * Version: 2.0.22-dev
  * Text Domain: redis-cache
  * Domain Path: /languages
  * Network: true
