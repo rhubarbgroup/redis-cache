d89603b67667933c079a64ca10702c3b790cda12
Till Krüss <tillkruss@users.noreply.github.com>
2025-09-26 13:17:33 -0700

    removed deprecated `WP_REDIS_SERIALIZER` constant

--- includes/object-cache.php
+++ includes/object-cache.php
@@ -753,12 +753,6 @@ class WP_Object_Cache {
 
             $this->diagnostics += $args;
         }
-
-        if ( defined( 'WP_REDIS_SERIALIZER' ) && ! empty( WP_REDIS_SERIALIZER ) ) {
-            $this->redis->setOption( Redis::OPT_SERIALIZER, WP_REDIS_SERIALIZER );
-
-            trigger_error('The `WP_REDIS_SERIALIZER` configuration constant has been deprecated in favor of `WP_REDIS_IGBINARY`', E_USER_DEPRECATED);
-        }
     }
 
     /**
@@ -826,12 +820,6 @@ class WP_Object_Cache {
             }
 
             $this->diagnostics += $args;
-        }
-
-        if ( defined( 'WP_REDIS_SERIALIZER' ) && ! empty( WP_REDIS_SERIALIZER ) ) {
-            $this->redis->setOption( Relay\Relay::OPT_SERIALIZER, WP_REDIS_SERIALIZER );
-
-            trigger_error('The `WP_REDIS_SERIALIZER` configuration constant has been deprecated in favor of `WP_REDIS_IGBINARY`', E_USER_DEPRECATED);
         }
     }
 
@@ -2796,10 +2784,6 @@ LUA;
      * @return mixed            Unserialized data can be any type.
      */
     protected function maybe_unserialize( $original ) {
-        if ( defined( 'WP_REDIS_SERIALIZER' ) && ! empty( WP_REDIS_SERIALIZER ) ) {
-            return $original;
-        }
-
         if ( defined( 'WP_REDIS_IGBINARY' ) && WP_REDIS_IGBINARY && function_exists( 'igbinary_unserialize' ) ) {
             return igbinary_unserialize( $original );
         }
@@ -2824,10 +2808,6 @@ LUA;
     protected function maybe_serialize( $data ) {
         if ( is_object( $data ) ) {
             $data = clone $data;
-        }
-
-        if ( defined( 'WP_REDIS_SERIALIZER' ) && ! empty( WP_REDIS_SERIALIZER ) ) {
-            return $data;
         }
 
         if ( defined( 'WP_REDIS_IGBINARY' ) && WP_REDIS_IGBINARY && function_exists( 'igbinary_serialize' ) ) {
