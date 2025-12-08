5129873fe9b245904dc53dba2669b06e956490da
Mickaël Perrin <contact@mickaelperrin.fr>
2025-12-08 13:40:47 +0100

    ✨ Add action hooks on disable and update-dropin events

--- includes/cli/class-commands.php
+++ includes/cli/class-commands.php
@@ -124,7 +124,18 @@ class Commands extends WP_CLI_Command {
 
                 WP_Filesystem();
 
-                if ( $wp_filesystem->delete( WP_CONTENT_DIR . '/object-cache.php' ) ) {
+                $result = $wp_filesystem->delete(WP_CONTENT_DIR . '/object-cache.php');
+
+                /**
+                 * Fires on cache disable event
+                 *
+                 * @param bool $result Whether the filesystem event (deletion of the `object-cache.php` file) was successful.
+                 *
+                 * @since 1.3.5
+                 */
+                do_action('redis_object_cache_disable', $result);
+
+                if ($result) {
                     $this->flush_redis();
 
                     WP_CLI::success( __( 'Object cache disabled.', 'redis-cache' ) );
@@ -159,6 +170,15 @@ class Commands extends WP_CLI_Command {
             true,
             FS_CHMOD_FILE
         );
+
+        /**
+         * Fires on cache update-dropin event
+         *
+         * @param bool $result Whether the filesystem event (copy of the `object-cache.php` file) was successful.
+         *
+         * @since 1.3.5
+         */
+        do_action('redis_object_cache_update_dropin', $copy);
 
         if ( $copy ) {
             $flush = $this->flush_redis();
